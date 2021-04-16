8532c8ef7609170835e79fa031f7005a2e147181
ignat5 <ignat-2000@mail.ru>
2021-04-16 13:17:42 +0300

    Finished RegistrationActivity; Started LoginActivity

--- app/src/main/java/com/example/messenger_siw/RegisterActivity.java
+++ app/src/main/java/com/example/messenger_siw/RegisterActivity.java
@@ -2,9 +2,13 @@ package com.example.messenger_siw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -40,6 +44,24 @@ public class RegisterActivity extends AppCompatActivity {
         //Firebase auth
         firebaseAuth = FirebaseAuth.getInstance();
 
+        //Adding event listener to BTN_register
+        BTN_register.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String username = ET_username.getText().toString();
+                String password = ET_password.getText().toString();
+                String email = ET_email.getText().toString();
+                //check input data
+                if(TextUtils.isEmpty(username) || TextUtils.isEmpty(password) ||TextUtils.isEmpty(email)) {
+                    Toast.makeText(RegisterActivity.this,"Fill your fields please",Toast.LENGTH_LONG).show();
+                } else {
+                    RegisterUser(username,password,email);
+                }
+
+            }
+        });
+
     }
 
     private void RegisterUser(final String username,String password,String email) {
@@ -56,17 +78,32 @@ public class RegisterActivity extends AppCompatActivity {
                     //hash maps
                     HashMap<String,String> hashMapUsers = new HashMap<>();
                     hashMapUsers.put("id",userID);
-                    hashMapUsers.put("username",ET_username.toString());
+                    hashMapUsers.put("username",username.toString());
                     hashMapUsers.put("imageURL","default");
 
                     //Open Main Activity after successful registration
+                    databaseReference.setValue(hashMapUsers)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(Task<Void> task) {
+                                    if(task.isSuccessful()) {
+                                        Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                    });
 
 
 
+                }else {
+                    Toast.makeText(RegisterActivity.this,"Invalid email or password",Toast.LENGTH_LONG).show();
                 }
             }
         });
 
     }
+    
 
 }
